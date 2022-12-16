feff950e513c6b758464287bbd910231eff9fd64
PVSwim <pavel.vlasenko65@gmail.com>
2022-12-16 20:43:13 +0300

    HW1 final

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -4,29 +4,32 @@
 //HW Task #2
 //Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-Console.Write("Input number 1 "); int number1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input number 2 "); int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input first number "); int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number "); int number2 = Convert.ToInt32(Console.ReadLine());
 
 //int max = Math.Max(number2, number1);//version 2
-int max = number2;
+int max = number1;
 
 if (number2 > max) max = number2;
 
 Console.WriteLine("Maximum is " + max);
 
+*/
+
 /*
 // HW Task 4
 //Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-Console.Write("Input number 1 "); int number1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input number 2 "); int number2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input number 3 "); int number3 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input first number "); int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number "); int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input third number  "); int number3 = Convert.ToInt32(Console.ReadLine());
 int max = number1;
 
 if (number2 > max) max = number2;
 if (number3 > max) max = number3;
 
 Console.WriteLine("Maximum is " + max);
+
 */
 
 /*
@@ -45,14 +48,18 @@ else
 }
 */
 
-
+/*
 // HW Task 8
 //Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
 Console.Write("Input number  ");
 int number1 = Convert.ToInt32(Console.ReadLine());
-int count = 2;
-if (number1 > 2)
+int count = 0;
+if ((number1 == 1) | ( number1 < 0))
+{
+    Console.WriteLine("No even numbers");
+}
+else
 {
     string resulteven = " Even numbers are ";
 
@@ -63,8 +70,4 @@ if (number1 > 2)
     }
     Console.WriteLine(resulteven);
 }
-else
-{
-    Console.WriteLine("No even numbers");
-}
-
+*/
